cdf70c398c7068438ea4e7538c0ccfd1f332e5a8
ashutoshsameer <sameerashutosh0201@gmail.com>
2021-11-27 12:33:53 -0500

    Dummy Profile

--- src/containers/MyAilments.js
+++ src/containers/MyAilments.js
@@ -1,9 +1,42 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import moment from 'moment';
 
 export default function MyAilments() {
+
+    const ailments = [
+        {
+            name: 'Diabetes',
+            otherData: {
+                addedOn: moment('11/27/2021', 'MM/DD/YYYY').format("MMMM Do YYYY")
+            }
+        }
+    ];
+
     return (
         <div>
-            My Ailments
+            {ailments.map((ail, index) => (
+                <Card sx={{ maxWidth: 275 }} raised>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            {ail.name}
+                        </Typography>
+                        <br/>
+                        <Typography variant="body2" color="text.secondary">
+                            added on
+                            <br />
+                            {ail.otherData.addedOn}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Track</Button>
+                    </CardActions>
+                </Card>
+            ))}
         </div>
     );
 }
--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,9 +1,97 @@
-import React from "react";
+import React, {useState} from "react";
+import {FormControl, InputAdornment, TextField, Typography} from "@mui/material";
+import {Container} from "@material-ui/core";
+import {Col, Row} from "react-bootstrap";
+import EditIcon from '@material-ui/icons/Edit';
 
 export default function Profile() {
+    const [values, setValues] = useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
     return (
-        <div>
-            Profile
-        </div>
+        <Container maxWidth="xs">
+            <Row>
+                <Col md={6}>
+                    <Typography color="text.secondary" style={{padding: '10px'}}>About You</Typography>
+                </Col>
+                <Col md={{span: 6, offset: 0}} style={{padding: '10px'}}>
+                    <EditIcon style={{float: 'right'}}/>
+                </Col>
+            </Row>
+            <FormControl sx={{width: '100%'}}>
+                <TextField size="small" id="outlined-basic" label="First Name" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+                <TextField size="small" id="outlined-basic" label="Last Name" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+                <TextField size="small" id="outlined-basic" label="Gender" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+                <TextField size="small" id="outlined-basic" label="Email" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+                <TextField size="small" id="outlined-basic" label="Phone Number" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+                <TextField size="small" id="outlined-basic" label="Area Code" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+            </FormControl>
+            <br/>
+            <br/>
+            <Row>
+                <Col md={6}>
+                    <Typography color="text.secondary" style={{padding: '10px'}}>Personal Details</Typography>
+                </Col>
+                <Col md={{span: 6, offset: 0}} style={{padding: '10px'}}>
+                    <EditIcon style={{float: 'right'}}/>
+                </Col>
+            </Row>
+            <FormControl sx={{width: '100%'}}>
+                <Row>
+                    <Col md={6}>
+                        <TextField size="small" id="outlined-basic" label="Date of Birth" variant="outlined"
+                                   style={{paddingBottom: '10px'}}/>
+                    </Col>
+                    <Col md={6}>
+                        <TextField size="small" id="outlined-basic" label="Blood Type" variant="outlined"
+                                   style={{paddingBottom: '10px'}}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                        <TextField size="small" id="outlined-basic" label="Height" variant="outlined"
+                                   style={{paddingBottom: '10px'}} InputProps={{
+                            endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+                        }}/>
+                    </Col>
+                    <Col md={6}>
+                        <TextField size="small" id="outlined-basic" label="Weight" variant="outlined"
+                                   style={{paddingBottom: '10px'}} InputProps={{
+                            endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+                        }}/>
+                    </Col>
+                </Row>
+            </FormControl>
+
+            <br/>
+            <br/>
+            <Row>
+                <Col md={6}>
+                    <Typography color="text.secondary" style={{padding: '10px'}}>Health History</Typography>
+                </Col>
+            </Row>
+            <FormControl sx={{width: '100%'}}>
+                <TextField size="small" id="outlined-basic" label="Ailments" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+                <TextField size="small" id="outlined-basic" label="Medication" variant="outlined"
+                           style={{paddingBottom: '10px'}}/>
+            </FormControl>
+
+        </Container>
     );
 }
