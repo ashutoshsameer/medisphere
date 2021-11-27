cc6efc8b783876d5d694968f796c14c6bf50a387
ashutoshsameer <sameerashutosh0201@gmail.com>
2021-11-26 19:25:12 -0500

    Doctor Search placeholder

--- src/containers/DoctorSearch.js
+++ src/containers/DoctorSearch.js
@@ -1,9 +1,31 @@
 import React from "react";
+import {Button, Card, Col, Row} from "react-bootstrap";
 
 export default function DoctorSearch() {
     return (
         <div>
-            Search Doctors
+            <Row>
+                <Col md={{ span: 6, offset: 3 }} style={{display: 'flex'}}>
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <Button>Go</Button>
+                </Col>
+            </Row>
+            <Row xs={1} md={3} className="g-4">
+                {Array.from({length: 4}).map((_, idx) => (
+                    <Col>
+                        <Card style={{marginTop: '15px'}}>
+                            {/*<Card.Img variant="top" src="holder.js/100px160" />*/}
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
 }
