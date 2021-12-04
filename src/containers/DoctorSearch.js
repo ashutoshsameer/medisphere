4c5dd58530ab809c4212a3f78f8e48e25c341253
Siddharth Jain <sj3132@columbia.edu>
2021-12-04 16:33:39 -0500

    added doctor search

--- src/containers/DoctorSearch.js
+++ src/containers/DoctorSearch.js
@@ -1,27 +1,59 @@
-import React from "react";
-import {Card, Col, Row} from "react-bootstrap";
+import React, { useState } from "react";
+import {Card, Col, Row, ListGroup} from "react-bootstrap";
 import {Button} from "@mui/material";
 
 export default function DoctorSearch() {
+
+    const [docs, setDocs] = useState([]);
+
+    async function awsCall(lat, lon, searchString, dist) {
+        const headers = { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'};
+        fetch('https://ujap4eccgg.execute-api.us-east-1.amazonaws.com/v1/doctors?lat='+lat+'&lon='+lon+'&searchString='+searchString+'&dist='+dist,
+        {
+            method: 'GET',
+            headers: headers,
+        })
+        .then(data=>data.json())
+        .then(data=>{
+            console.log(data)
+            setDocs(data)
+        });
+    }
+
+    async function searchDoctors(searchString) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+                awsCall(position.coords.latitude, position.coords.longitude, searchString, "100km")
+            }, function(position){
+                awsCall(43.295, -75.577, searchString, "100km")
+            });
+          } else { 
+            console.log('No location service')
+          }
+    }
+
     return (
         <div>
             <Row>
                 <Col md={{ span: 6, offset: 3 }} style={{display: 'flex'}}>
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <Button>Go</Button>
+                    <input id="searchString" className="form-control mr-sm-2" type="search" placeholder="Search for speciality" aria-label="Search"/>
+                    <Button onClick={() => {
+                        searchDoctors(document.getElementById('searchString').value)
+                    }}>Go</Button>
                 </Col>
             </Row>
             <Row xs={1} md={3} className="g-4">
-                {Array.from({length: 4}).map((_, idx) => (
-                    <Col key={`result_${idx}`}>
+                {docs.map(doctor => (
+                    <Col key={`result`}>
                         <Card style={{marginTop: '15px'}}>
                             {/*<Card.Img variant="top" src="holder.js/100px160" />*/}
                             <Card.Body>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
-                                    This is a longer card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.
-                                </Card.Text>
+                                <Card.Title id="card_title">{doctor.name}</Card.Title>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>{doctor.speciality}</ListGroup.Item>
+                                    <ListGroup.Item>{doctor.contact}</ListGroup.Item>
+                                    <ListGroup.Item>{doctor.address}</ListGroup.Item>
+                                </ListGroup>
                             </Card.Body>
                         </Card>
                     </Col>
